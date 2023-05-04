51721a695d41df9490fd857b4f1f41aedbc896c5
Jane Tung <22017723@myrp.edu.sg>
2023-05-04 11:26:38 +0800

    Completed up to Section C

--- app/src/main/java/sg/edu/rp/c346/id22017723/week03/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22017723/week03/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.id22017723.week03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -22,5 +23,15 @@ public class MainActivity extends AppCompatActivity {
         tvDisplay = findViewById(R.id.TextViewDisplay);
         btnDisplay = findViewById(R.id.buttonDisplay);
         etInput = findViewById(R.id.EditTextInput);
+
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stringResponse = etInput.getText().toString();
+                tvDisplay.setText(stringResponse);
+
+            }
+        });
+
     }
 }
